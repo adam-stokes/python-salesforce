0f7dccb5439478d14e4f3d712f2ba02849a57f41
Adam Stokes <hackr@cypherbook.com>
2013-03-18 11:36:05 -0400

    Add ssl server for exchanging auth tokens
    Signed-off-by: Adam Stokes <hackr@cypherbook.com>

--- sforce/auth.py
+++ sforce/auth.py
@@ -1,15 +1,19 @@
 import requests
 from requests_oauthlib import OAuth1
 from urlparse import parse_qs
+from pprint import pprint
 
 class Connect(object):
-    def __init__(self, client_key, client_secret, sandbox=False):
+    def __init__(self, client_key=None, client_secret=None,
+                 callback_uri=None, sandbox=False):
         self.client_key = client_key
         self.client_secret = client_secret
+        self.callback_uri = callback_uri
         self.request_path = "/_nc_external/system/security/oauth/RequestTokenHandler"
         self.authorize_path = "/setup/secur/RemoteAccessAuthorizationPage.apexp"
         self.access_path = "/_nc_external/system/security/oauth/AccessTokenHandler"
-        self.oauth = OAuth1(self.client_key, client_secret=self.client_secret)
+        self.oauth = OAuth1(self.client_key, client_secret=self.client_secret,
+                            callback_uri=self.callback_uri)
         self.oauth_token = None
         self.oauth_token_secret = None
         self.sandbox = sandbox
@@ -30,11 +34,9 @@ class Connect(object):
 
     def authorize(self):
         """ Authorize phase """
-        authorize_url_root = self._root_url + self.authorize_path 
+        authorize_url_root = self._root_url + self.authorize_path
         authorize_url_query = "?oauth_token=" + self.oauth_token + "&oauth_consumer_key=" + self.client_key
         authorize_url = authorize_url_root + authorize_url_query
-        # TODO: Add either https callback or manual addition of verifier token
-        return
 
     def access(self, verifier):
         """ Access phase """
