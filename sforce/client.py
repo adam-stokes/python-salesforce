30a5580aae62907526b5849e81faf68b54e0f9bb
Adam Stokes <hackr@cypherbook.com>
2013-03-19 14:58:06 -0400

    work on client
    Signed-off-by: Adam Stokes <hackr@cypherbook.com>

--- sforce/client.py
+++ sforce/client.py
@@ -1,20 +1,39 @@
 import requests
+from requests_oauthlib import OAuth1
+from urlparse import parse_qs, urlunparse
+import os
+import json
+from pprint import pprint
 
 class SF(object):
     """ SF Client class """
-    def __init__(self, client_key, token):
-        self.client_key = client_key
-        self.token = token
+    def __init__(self):
+        self.creds = json.load(open(os.path.join(os.path.expanduser("~"), ".sfcreds.json")))
+        pprint(self.creds)
+        self.api_url = "https://login.salesforce.com/services/OAuth/u/27.0"
 
     def _call(self):
         """ Performs GET/POST/PATCH/PUT/DELETE
             requests
         """
-        pass
-
+        oauth = OAuth1(os.environ["SFKEY"],
+                       client_secret=os.environ["SFSECRET"],
+                       resource_owner_key=self.creds['oauth_token'],
+                       resource_owner_secret=self.creds['oauth_token_secret'])
+        r = requests.post(url=self.api_url, auth=oauth)
+        pprint(r.content)
+        credentials = parse_qs(r.content)
+ 
     def request(self, method, query):
         """ Formulates a proper request
             based on intended verb
         """
         pass
 
+
+"""
+Test caller
+"""
+if __name__=="__main__":
+    sfapi = SF()
+    sfapi._call()
